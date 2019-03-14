45c3c6ad6d1c37c0f10f6d947c7f90c0705e041c
moldabayev_v <moldabayev.v@chocolife.kz>
2019-03-14 11:58:51 +0600

    Исключение параметром из ключа для кэша

--- src/Middleware/CacheMiddleware.php
+++ src/Middleware/CacheMiddleware.php
@@ -8,6 +8,7 @@ namespace Chocofamily\SmartHttp\Middleware;
 
 use Chocofamily\SmartHttp\Http\Request;
 use Chocofamily\SmartHttp\Http\Response;
+use function Chocofamily\SmartHttp\unparse_url;
 use GuzzleHttp\Promise\Promise;
 use function GuzzleHttp\Promise\promise_for;
 use GuzzleHttp\Psr7\Response as PsrResponse;
@@ -18,6 +19,11 @@ class CacheMiddleware
 {
     const DEFAULT_KEY_PREFIX  = 'smarthttp_';
     const SUCCESS_STATUS_CODE = 200;
+    const EXCLUDED_PARAMS     = [
+        'correlation_id',
+        'span_id',
+    ];
+
     private $cache;
 
     public function __construct(BackendInterface $cache)
@@ -30,8 +36,7 @@ class CacheMiddleware
         return function (RequestInterface $request, array $requestOptions) use ($handler) {
             $lifetime = $requestOptions[Request::CACHE_LIFETIME] ?? null;
             $prefix   = $requestOptions[Request::CACHE_PREFIX] ?? '';
-            $key      = $request->getMethod() !== 'GET' ? null
-                : $this->getKey($request->getMethod(), $request->getUri(), $prefix);
+            $key      = $lifetime ? $this->getKey($request->getMethod(), $request->getUri(), $prefix) : '';
 
             if ($this->isCacheable($key, $lifetime)
                 && !empty($data = $this->cache->get($key))) {
@@ -58,11 +63,56 @@ class CacheMiddleware
 
     private function getKey(string $method, string $uri, string $prefix = ''): string
     {
+        if ($method !== 'GET') {
+            return '';
+        }
+
         if (empty($prefix)) {
             $prefix = self::DEFAULT_KEY_PREFIX;
         }
 
+        $uri = $this->clearUrl($uri);
+
         return $prefix.md5($method.$uri);
+    }
+
+    /**
+     * Очищает от лищних query параметоров
+     *
+     * @param string $url
+     *
+     * @return string
+     */
+    private function clearUrl(string $url): string
+    {
+        if (strpos($url, '?') === false) {
+            return $url;
+        }
+
+        $parseUrl = parse_url($url);
+        if (false == isset($parseUrl['query'])) {
+            return $url;
+        }
+
+        if (strpos($parseUrl['query'], self::EXCLUDED_PARAMS[0]) === false) {
+            return $url;
+        }
+
+        $parseUrl['query'] = htmlspecialchars_decode($parseUrl['query']);
+        parse_str($parseUrl['query'], $parseQuery);
+
+        foreach (self::EXCLUDED_PARAMS as $param) {
+            if (isset($parseQuery[$param])) {
+                unset($parseQuery[$param]);
+            }
+        }
+        if ($parseQuery) {
+            $parseUrl['query'] = http_build_query($parseQuery);
+        } else {
+            unset($parseUrl['query']);
+        }
+
+        return unparse_url($parseUrl);
     }
 
     private function getResponseFromCache($data)
